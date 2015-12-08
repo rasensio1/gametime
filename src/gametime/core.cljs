ab205df49ebf91e43ed79ec318630697229bbc41
Ryan Asensio <ryan.asensio@gmail.com>
2015-12-08 15:22:32 -0700

    renders a square and clears the canvas

--- src/gametime/core.cljs
+++ src/gametime/core.cljs
@@ -16,31 +16,17 @@
 (defn go-down [[x y]] [x (dec y)])
 (defn go-up [[x y]] [x (inc y)])
 
-(defn hello-world []
-  [:h1 (:text @app-state)])
+(defonce context (.getContext (.getElementById js/document "target") "2d" ))
 
-(r/render-component [hello-world]
-                         (. js/document (getElementById "app")))
 
-(defn simple-component []
-    [:div
-    [:p "I am a component!"]
-    [:p.someclass
-        "I have " [:strong "bold"]
-    [:span {:style {:color "red"}} " and red "] "text."]])
+(defn drawSquare [x y w h] (.fillRect context x y w h))
+(defn clearSquare [] (.clearRect context 0 0 500 500))
 
-(defn render-simple []
-  (r/render-component [simple-component]
-                      (.-body js/document)))
+(clearSquare)
+(drawSquare 0 0 100 100)
 
-(render-simple)
-
-(def initial-state {:board empty-board
-                    :position [9 0]
-                    :direction go-right})
 
 (defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+  (println "reloaded")
 )
+
