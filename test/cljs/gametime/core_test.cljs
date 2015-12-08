7260e9cb3250c05f5aebc89694b63824a3f47518
Ryan Asensio <ryan.asensio@gmail.com>
2015-12-07 20:36:41 -0700

    run tests from file

--- test/cljs/gametime/core_test.cljs
+++ test/cljs/gametime/core_test.cljs
@@ -1,14 +1,27 @@
-(ns ^:figwheel-always hickey48.core-test
+(ns ^:figwheel-always gametime.core-test
     (:require
       [cljs.test :refer-macros [deftest testing is]]
-      [hickey48.test-formatter :as formatter]
+      [gametime.test-formatter :as formatter]
       [figwheel.client :as fw]
       [dommy.core :refer-macros [sel sel1]]
-      [hickey48.test-helpers :as th]
-      [hickey48.core :as c]))
+      [gametime.test-helpers :as th]
+      [gametime.core :as c]))
 
 (enable-console-print!)
 
 (deftest test-is_true
     (is true))
+
+(defn run-tests []
+    (.clear js/console)
+      (cljs.test/run-all-tests #"hickey48.*-test"))
+(run-tests)
+
+;; FW connection is optional in order to simply run tests,
+;; but is needed to connect to the FW repl and to allow
+;; auto-reloading on file-save
+(fw/start {
+           :websocket-url "ws://localhost:3449/figwheel-ws"
+           :build-id "test"
+           })
 
